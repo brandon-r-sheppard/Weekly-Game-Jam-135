71639339da730e230d180edb6e6c9608db628311
Oana Furtuna <oana.furtuna98@gmail.com>
2020-02-08 19:41:03 +0000

    Added Idle, Follow and Patrol states

---
+++ Assets/AI/AIController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    public float MoveSpeed = 5;
+    public float TurnSpeed = 3;
+
+    private AIFieldOfView fieldOfView;
+    public NavMeshAgent NavAgent;
+
+    public Transform CurrentTarget
+    {
+        get
+        {
+            return fieldOfView.PlayerTarget;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavAgent = GetComponent<NavMeshAgent>();
+        fieldOfView = GetComponent<AIFieldOfView>();
+        NavAgent.speed = MoveSpeed;
+        NavAgent.angularSpeed = TurnSpeed;
+    }
+
+    // Update is called once per frame
+    public void UpdateRotation()
+    {
+        var moveDirection = NavAgent.destination - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), TurnSpeed * Time.deltaTime);
+        
+    }
+
+
+}
---
+++ Assets/AI/AIIdleBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIIdleBehavior : StateMachineBehaviour
+{
+    float idleTime = 2.0f;
+    float currentTime = 0.0f;
+
+    private AIController controller;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        controller = animator.gameObject.GetComponent<AIController>();
+        Debug.Log("Idle");
+        idleTime = Random.Range(0.5f, 3.0f);
+        currentTime = 0.0f;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        currentTime += Time.deltaTime;
+        animator.SetBool("mIsFollowing", controller.CurrentTarget != null);
+        animator.SetBool("mIsPatrolling", currentTime >= idleTime);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+    }
+
+}
